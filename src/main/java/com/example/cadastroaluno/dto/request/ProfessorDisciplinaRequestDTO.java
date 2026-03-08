f3a4a1c8411b2935c1971bc93c08cd99b93ddd74
lorenatavares-ieg <lorena.tavares@germinare.org.br>
2026-03-08 16:09:40 -0300

    Arrumando auth e service professorDisciplina

--- src/main/java/com/example/cadastroaluno/dto/request/ProfessorDisciplinaRequestDTO.java
+++ src/main/java/com/example/cadastroaluno/dto/request/ProfessorDisciplinaRequestDTO.java
@@ -1,8 +1,13 @@
 package com.example.cadastroaluno.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class ProfessorDisciplinaRequestDTO {
     @NotNull(message = "O professor é obrigatório")
     private Integer professorId;
+
+    @NotNull(message = "A disciplina é obrigatório")
+    private Integer disciplinaId;
 }
--- src/main/java/com/example/cadastroaluno/service/ProfessorDisciplinaService.java
+++ src/main/java/com/example/cadastroaluno/service/ProfessorDisciplinaService.java
@@ -1,9 +1,14 @@
 package com.example.cadastroaluno.service;
 
+import com.example.cadastroaluno.dto.request.ProfessorDisciplinaRequestDTO;
 import com.example.cadastroaluno.dto.response.ProfessorDisciplinaResponseDTO;
+import com.example.cadastroaluno.model.Disciplina;
+import com.example.cadastroaluno.model.Professor;
 import com.example.cadastroaluno.model.ProfessorDisciplina;
 import com.example.cadastroaluno.model.ProfessorDisciplinaId;
+import com.example.cadastroaluno.repository.DisciplinaRepository;
 import com.example.cadastroaluno.repository.ProfessorDisciplinaRepository;
+import com.example.cadastroaluno.repository.ProfessorRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +20,46 @@ import java.util.List;
 public class ProfessorDisciplinaService {
 
     private final ProfessorDisciplinaRepository professorDisciplinaRepository;
+    private final ProfessorRepository professorRepository;
+    private final DisciplinaRepository disciplinaRepository;
+
+    private ProfessorDisciplina toEntity(ProfessorDisciplinaRequestDTO dto) {
+
+        Professor professor = professorRepository.findById(dto.getProfessorId())
+                .orElseThrow(() -> new EntityNotFoundException("Professor não encontrado"));
+
+        Disciplina disciplina = disciplinaRepository.findById(dto.getDisciplinaId())
+                .orElseThrow(() -> new EntityNotFoundException("Disciplina não encontrada"));
+
+        ProfessorDisciplina pd = new ProfessorDisciplina();
+        pd.setProfessor(professor);
+        pd.setDisciplina(disciplina);
+
+        return pd;
+    }
 
     private ProfessorDisciplinaResponseDTO toResponseDTO(ProfessorDisciplina entity) {
+
         ProfessorDisciplinaResponseDTO dto = new ProfessorDisciplinaResponseDTO();
 
-        dto.setDisciplinaId(entity.getDisciplina().getIdDisciplina());
         dto.setProfessorId(entity.getProfessor().getIdProfessor());
+        dto.setDisciplinaId(entity.getDisciplina().getIdDisciplina());
 
         return dto;
+    }
+
+    public ProfessorDisciplinaResponseDTO vincular(ProfessorDisciplinaRequestDTO dto) {
+
+        if (professorDisciplinaRepository.existsByProfessor_IdProfessorAndDisciplina_IdDisciplina(
+                dto.getProfessorId(), dto.getDisciplinaId())) {
+
+            throw new RuntimeException("Esse professor já está vinculado a essa disciplina");
+        }
+
+        ProfessorDisciplina pd = toEntity(dto);
+        pd = professorDisciplinaRepository.save(pd);
+
+        return toResponseDTO(pd);
     }
 
     public List<ProfessorDisciplinaResponseDTO> listarTodos() {
@@ -32,4 +69,17 @@ public class ProfessorDisciplinaService {
                 .toList();
     }
 
+    public void remover(Integer professorId, Integer disciplinaId) {
+
+        ProfessorDisciplinaId id = new ProfessorDisciplinaId(disciplinaId, professorId);
+
+        if (!professorDisciplinaRepository.existsById(id)) {
+            throw new EntityNotFoundException(
+                    "Vínculo entre professor " + professorId +
+                            " e disciplina " + disciplinaId + " não encontrado"
+            );
+        }
+
+        professorDisciplinaRepository.deleteById(id);
+    }
 }
